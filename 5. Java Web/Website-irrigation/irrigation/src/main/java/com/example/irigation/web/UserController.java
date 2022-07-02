dd1ba1f3b6cac242ef2d50a3977d424c71d50755
Stefan Stoev <stoev.stefan@gmail.com>
2022-07-03 00:29:36 +0300

    index
    Added Index.html

---
+++ 5. Java Web/Website-irrigation/irrigation/src/main/java/com/example/irigation/web/UserController.java
@@ -0,0 +1,21 @@
+package com.example.irigation.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/users")
+public class UserController {
+
+    @GetMapping("/login")
+    private String login(){
+        return "index";
+    }
+
+    @GetMapping("/register")
+    private String register(){
+        return "index";
+    }
+
+}
